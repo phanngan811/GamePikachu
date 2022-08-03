6c67cf7bba0f89406e0850873ffc0ec2af113fb7
ngan <s3863893@rmit.edu.vn>
2022-08-03 09:35:18 +0700

    Initial Commit

---
+++ GamePikachu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GamePikachu
+//
+//  Created by Ngan Phan Thi Thu on 03/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GamePikachu/GamePikachuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePikachuApp.swift
+//  GamePikachu
+//
+//  Created by Ngan Phan Thi Thu on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GamePikachuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
